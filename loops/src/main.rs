08d6c946be6c0f0f8e5ac494891df3f53791b5d3
diogobnfr <diogobonofre@gmail.com>
2024-10-26 13:57:32 -0300

    feat: basic loop concepts

---
+++ loops/src/main.rs
@@ -0,0 +1,14 @@
+#[allow(unused_labels)]
+fn returning_from_loop() -> u32 {
+    let mut counter: u32 = 0;
+    'return_loop: loop { // '<loop label>:
+        counter += 1;
+        if counter == 10 {
+            break counter; // This is the return value
+        }
+    }
+}
+
+fn main() {
+    println!("{}", returning_from_loop())
+}
